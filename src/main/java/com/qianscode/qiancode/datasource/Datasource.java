2f255af073caafeefad4fc3ff34f77e8f5707133
qian3333 <1459114005@qq.com>
2024-07-23 15:00:53 +0800

    prepare work
    steps to do before install flyway and make migrations

--- src/main/java/com/qianscode/qiancode/datasource/Datasource.java
+++ src/main/java/com/qianscode/qiancode/datasource/Datasource.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class Datasource {
 
     @Bean
-    
     @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource() {
         return DataSourceBuilder
